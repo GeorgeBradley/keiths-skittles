07e5209d60ef67b2a17b33f07faaf5bebe26067f
George Bradley <georgeaustinbradley@outlook.com>
2025-03-24 17:20:49 +0000

    Add root URL redirect to /live/

--- skittles_app/urls.py
+++ skittles_app/urls.py
@@ -3,7 +3,6 @@ from django.urls import path
 from scores import views
 from django.shortcuts import redirect
 
-# Redirect root URL to /live/
 def redirect_to_live(request):
     return redirect('start_live_game')
 
@@ -12,5 +11,5 @@ urlpatterns = [
     path('live/<int:game_id>/', views.live_game, name='live_game'),
     path('live/', views.live_game, name='start_live_game'),
     path('add-player/', views.add_player, name='add_player'),
-    path('', redirect_to_live, name='root'),  # Add this line for root URL
+    path('', redirect_to_live, name='root'),
 ]
