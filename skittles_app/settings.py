adc7d570992c0a190ce7f916eab2787b5e957dd0
George Bradley <georgeaustinbradley@outlook.com>
2025-03-24 00:19:19 +0000

    Initial commit of Keith’s Skittles app

--- skittles_app/settings.py
+++ skittles_app/settings.py
@@ -11,7 +11,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
-
+import os
+import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -78,7 +79,7 @@ WSGI_APPLICATION = 'skittles_app.wsgi.application'
 DATABASES = {
     'default': dj_database_url.config(default='sqlite:///db.sqlite3')
 }
-
+DATABASES['default']['OPTIONS'] = {'sslmode': 'require'} 
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
